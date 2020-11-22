fce059f49abb250c7f0326fbe7dd9eaa07bbd1a0
Kesheon <74798125+Kesheon@users.noreply.github.com>
2020-11-22 02:00:12 -0600

    added characterinventory2.py

---
+++ characterinventory2.py
@@ -0,0 +1,14 @@
+
+
+class CharacterInventory2:
+    def __init__(self, character_inventory2):
+        self.__character_inventory2 = character_inventory2
+
+    def set_character_inventory2(self, character_inventory2):
+        self.__character_inventory2 = character_inventory2
+
+    def get_character_inventory2(self):
+        return self.__character_inventory2
+
+    def __str__(self):
+        return self.__character_inventory2
