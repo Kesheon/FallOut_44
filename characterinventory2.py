45e31679ae93543141743fe177c1bfb39dedbfa7
Kesheon <74798125+Kesheon@users.noreply.github.com>
2020-11-27 19:28:12 -0600

    added 3 comments explaining this class

--- characterinventory2.py
+++ characterinventory2.py
@@ -1,5 +1,7 @@
 
-
+# this class' purpose is to help in making
+# the character_inventory2 list that is in
+# the FallOut44.py file/program
 class CharacterInventory2:
     def __init__(self, character_inventory2):
         self.__character_inventory2 = character_inventory2
